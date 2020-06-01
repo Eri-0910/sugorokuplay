2f115c157bacf0e1caaa24dfc3e9924a10b3a9ce
YAMAKAWA ERINA <yamakawa.erina.43z@st.kyoto-u.ac.jp>
2020-06-01 22:48:27 +0900

    edit stockmessage action

--- src/restmessage.ts
+++ src/restmessage.ts
@@ -1,54 +1,66 @@
 /**
  * 残っているメッセージを送れるだけ取得する
  * @param userId ユーザーID
+ * @return 出力するもの
  */
-function getNextMessage(userId: string):string[]{
-    //ユーザーのシートを手に入れる
+function getNextMessage(userId: string):Object[]{
+    //　ユーザーのシートを手に入れる
     var SpreadSheet = getSpreadSheet(userId);
-    // これで１枚のシートを取得
+    // ゲームデータのシートを取得
     var dataSheet = SpreadSheet.getSheetByName(GAME_DATA_SHEET_NAME);
 
     //現在の状態を知る
     var leftMessageNum = dataSheet.getRange(NEXT_TEXT_NUM_RANGE).getValue();
 
     //今から送るメッセージのかずと、残りのメッセージの数を決める
-    var replyMessageNum;
-    if (leftMessageNum > 5){
+    var replyMessageNum: number;
+    if (leftMessageNum > 5){//6件以上あるなら4件だけ出力
         dataSheet.getRange(NEXT_TEXT_NUM_RANGE).setValue(leftMessageNum-4);
         replyMessageNum = 4;
-    } else{
+    } else{//5件以下なら全部出力
         dataSheet.getRange(NEXT_TEXT_NUM_RANGE).setValue(0);
         replyMessageNum = leftMessageNum;
     }
 
     //メッセージ決め
-    var replyMessages = [];
+    var replyMessages:Object[] = [];
     if (replyMessageNum == 0){
-        replyMessages.push('そのコマンドは無効です');
+        replyMessages.push(stringToMessage('そのコマンドは無効です'));
     } else{
       for(let i=0; i < replyMessageNum; i++){
-        const nextText = dataSheet.getRange('D1').getValue();
-        replyMessages.push(nextText);
-        dataSheet.getRange(NEXT_TEXT_COLUMN + '1').deleteCells(SpreadsheetApp.Dimension.ROWS);
+        const range:string = NEXT_TEXT_COLUMN + '1';
+        const nextText = dataSheet.getRange(range).getValue();
+        replyMessages.push(JSON.parse(nextText));
+        dataSheet.getRange(range).deleteCells(SpreadsheetApp.Dimension.ROWS);
       }
     }
     return replyMessages;
 }
 
-function setNextMessage(userId, lst){
-    //ユーザーのシートを手に入れる
+/**
+ * 次に出力するメッセージをJSONを文字列にしたもので保存
+ * @param userId ユーザーID
+ * @param lst 保存したいもののリスト
+ */
+function setNextMessage(userId: string, lst: Object[]){
+    //　ユーザーのシートを手に入れる
     var SpreadSheet = getSpreadSheet(userId);
-    // これで１枚のシートを取得
+    // ゲームデータのシートを取得
     var dataSheet = SpreadSheet.getSheetByName(GAME_DATA_SHEET_NAME);
 
-    //現在の状態を知る
-    var leftMessageNum = dataSheet.getRange(NEXT_TEXT_NUM_RANGE).getValue();
+    // 現在の状態を知る
+    var leftMessageNum:number= dataSheet.getRange(NEXT_TEXT_NUM_RANGE).getValue();
+    // 残っているコメントの数を更新
     dataSheet.getRange(NEXT_TEXT_NUM_RANGE).setValue(leftMessageNum + lst.length);
 
-   for (let i = 0; i < lst.length; i++) {
-            const writeRow = leftMessageNum + i + 1;
-            dataSheet.getRange(NEXT_TEXT_COLUMN + writeRow).setValue(lst[i]);
-   }
+    // 1つずつ保存
+    for (let i = 0; i < lst.length; i++) {
+        //書き込み行
+        const writeRow: number = leftMessageNum + i + 1;
+        //書き込む内容の変換
+        var writeData:string = JSON.stringify(lst[i]);
+        dataSheet.getRange(NEXT_TEXT_COLUMN + writeRow).setValue(writeData);
+    }
 }
 
 /**
@@ -56,7 +68,7 @@ function setNextMessage(userId, lst){
  * @param {String} userId ユーザーID
  * @return {boolean} あるかどうか
  */
-function hasNextMessage(userId) {
+function hasNextMessage(userId: string): boolean {
     //ユーザーのシートを手に入れる
     var SpreadSheet = getSpreadSheet(userId);
     // ゲーム状況のシートを取得
@@ -64,7 +76,7 @@ function hasNextMessage(userId) {
     //現在の残りメッセージ数を知る
     var leftMessageNum = dataSheet.getRange(NEXT_TEXT_NUM_RANGE).getValue();
     //返り値設定
-    var hasNext;
+    var hasNext: boolean;
     if (leftMessageNum==0){//残りメッセージなし
         hasNext = false;
     }else{//あり
